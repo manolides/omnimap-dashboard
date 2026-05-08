42f51979985e412b19d9ae73213998c7157d009e
Matt Manolides <manolides@gmail.com>
2026-05-08 10:41:43 -0700

    Revert map pool UI, move Korean maps to Open Tabs

--- config.js
+++ config.js
@@ -15,9 +15,6 @@ export const CONFIG = {
         mapbox: 'pk.eyJ1Ijoic3BhdGhpaW5jIiwiYSI6ImNtbzM3ZjB0bzA2dWkzeG95NGF4cGtqYTIifQ.7z0llzAjpjOenLDubOkyuA',
         azureMaps: 'ObnTb2AQpH5lrnuugbltiS0VnxYFobscLcLgM2vwHivHfzRxdXZ8JQQJ99CDACYeBjFt4bvcAAAgAZMP12HM',
         yandexMaps: '3e1ee434-1f9a-4b31-a8e6-3758d332e773',
-        appleJwtToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ijc3UFo1WTJXWjciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJVQjNTNTI4WURDIiwiaWF0IjoxNzc2NDQyMzUyLCJleHAiOjE4MDc5NzgzNTJ9.e5K7bNYkefJcxG-v9fSQGv5kUUtIu_hrSQCtE-0VSGV-VZK-xgF5EKqVOMYEfa74PIYXTj8-7IE0CDICJVepMw', // Valid for 1 year
-        naverMaps: 'YOUR_NAVER_CLIENT_ID', // Replace with your Naver Cloud Client ID
-        kakaoMaps: 'YOUR_KAKAO_APP_KEY', // Replace with your Kakao JavaScript App Key
-        tMaps: 'YOUR_TMAP_APP_KEY' // Replace with your TMap App Key
+        appleJwtToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ijc3UFo1WTJXWjciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJVQjNTNTI4WURDIiwiaWF0IjoxNzc2NDQyMzUyLCJleHAiOjE4MDc5NzgzNTJ9.e5K7bNYkefJcxG-v9fSQGv5kUUtIu_hrSQCtE-0VSGV-VZK-xgF5EKqVOMYEfa74PIYXTj8-7IE0CDICJVepMw' // Valid for 1 year
     }
 };
